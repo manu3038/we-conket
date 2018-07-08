d26af2c9be941dba4dc18c59915d53a4ba75201e
manu3038 <manohar.ng30@gmail.com>
2018-07-08 21:44:58 +0530

    updated database

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'weconekt',
-	'dbdriver' => 'mysqli',
+	'hostname' => 'ec2-107-21-95-70.compute-1.amazonaws.com',
+	'username' => 'moxpixwglhiyit',
+	'password' => '03bb18945abdd3780bab032f8f4f2f4f0ca9d5c8ec98f6a012dcdddfd8125919',
+	'database' => 'd2h4i837jhle5d',
+	'dbdriver' => 'pgsql',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
